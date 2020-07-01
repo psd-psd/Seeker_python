6d1471f6dad3066cfcb8a1273271dabfcfa48a35
prasad <prasad2565@gmail.com>
2020-07-01 02:40:58 -0400

    Update primeNumber.py

--- primeNumber.py
+++ primeNumber.py
@@ -1,14 +1,13 @@
-n=int(input('enter the max range '))
+n=int(input('enter the number '))
 a=0
 b=2
 for i in range (b, n):
     a=n%b
     if a==0:
-        print('enter the number ')
+        print('the number is not prime ')
         break
     else:
         if a!=0:
             b=b+1
             if b==n-1:
                 print('the number is prime')
-                
