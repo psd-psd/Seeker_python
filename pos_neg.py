5de12d870f96d40b78951f4a2e71c33e3a15a56f
Shepherd <prasad2565@gmail.com>
2021-07-18 16:57:27 -0400

    Update pos_neg.py

--- pos_neg.py
+++ pos_neg.py
@@ -26,3 +26,23 @@ def pos_neg(a, b, negative):
                 return True
         if b>0:
             return False
+
+        
+        
+# second method
+
+def pos_neg(a, b, negative):
+    if negative is True:
+        if a<0 and b<0:
+            return True
+        elif a<0 or b<0:
+            return False
+        else:
+            return False
+    if negative is False:
+        if a<0 and b<0:
+            return False
+        elif a<0 or b<0:
+            return True
+        else:
+            return False
