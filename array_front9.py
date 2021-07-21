bf6d49e7ccbcb08de2ba1fc183b60632133e81db
Shepherd <prasad2565@gmail.com>
2021-07-21 02:57:22 -0400

    Create array_front9.py

---
+++ array_front9.py
@@ -0,0 +1,19 @@
+# Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+# array_front9([1, 2, 9, 3, 4]) → True
+# array_front9([1, 2, 3, 4, 9]) → False
+# array_front9([1, 2, 3, 4, 5]) → False
+
+def array_front9(nums):
+    c=0
+    if len(nums)==0:
+            return False
+    else:
+        for i in nums:
+            c+=1
+            if i ==9 :
+                return True
+            if c==4:
+                return False
+        return False
