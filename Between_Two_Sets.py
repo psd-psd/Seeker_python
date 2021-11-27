c4ff38a69224eaa6f918787266e179fc27d297aa
prasad <prasad2565@gmail.com>
2021-11-28 01:41:59 +0530

    Update Between_Two_Sets.py

--- Between_Two_Sets.py
+++ Between_Two_Sets.py
@@ -1,4 +1,4 @@
-# https://hackerrank-challenge-pdfs.s3.amazonaws.com/26081-between-two-sets-English?AWSAccessKeyId=AKIAR6O7GJNX5DNFO3PV&Expires=1638047041&Signature=PMqFbLf2Q3LY8ZxoJJgToyIJIcw%3D&response-content-disposition=inline%3B%20filename%3Dbetween-two-sets-English.pdf&response-content-type=application%2Fpdf
+https://hackerrank-challenge-pdfs.s3.amazonaws.com/26081-between-two-sets-English?AWSAccessKeyId=AKIAR6O7GJNX5DNFO3PV&Expires=1638047041&Signature=PMqFbLf2Q3LY8ZxoJJgToyIJIcw%3D&response-content-disposition=inline%3B%20filename%3Dbetween-two-sets-English.pdf&response-content-type=application%2Fpdf
 
 
 
@@ -15,6 +15,3 @@ def getTotalX(a, b):
                 else:
                      c+=1
         return c
-      
-      
-      
