5836e7a0947b82124b62e24708d2967d1b589850
prasad <prasad2565@gmail.com>
2022-03-18 20:32:42 +0530

    Create Drawing Book.py

---
+++ Drawing Book.py
@@ -0,0 +1,18 @@
+#https://hackerrank-challenge-pdfs.s3.amazonaws.com/22564-drawing-book-English?AWSAccessKeyId=AKIAR6O7GJNX5DNFO3PV&Expires=1647619301&Signature=UOmI5Q3SrpiYy%2FIn2iptuYbevAc%3D&response-content-disposition=inline%3B%20filename%3Ddrawing-book-English.pdf&response-content-type=application%2Fpdf
+
+def pageCount(n, p):
+    if n-p>=p:
+        if p-1>=2:
+            return (p)//2
+        elif p-1==0:
+            return (0)
+        else:
+            return (1)
+    else:
+        if n-p<=p:
+            if n-p>=2:
+                return ((n-p)//2)
+            elif n-p==1:
+                return (1)
+            else:
+                return (0)
