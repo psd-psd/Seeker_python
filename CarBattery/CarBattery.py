1d7a71e630139fd02f03bb781815622582859160
prasad <prasad2565@gmail.com>
2022-03-26 13:56:24 +0530

    Create CarBattery.py

---
+++ CarBattery/CarBattery.py
@@ -0,0 +1,8 @@
+def drive(x):
+    p=(100-x//6)/100*100
+    if p>=20:
+        print('The battery percentage remaning is',p,'%',"and you are good to go")
+    elif p<=0:
+        print('you have run out of charge and the vehicle cant move')
+    else:
+        print('Alert!! The battery percentage is', p,'%',"too low please recharge")
